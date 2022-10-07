4a12edaf0c658707a0a24a181c18a9d5f4311d30
Mathis Scholz <mail@mathis-scholz.de>
2022-10-07 19:04:44 +0200

    added DF-Player and LEDs

--- Buzzer.ino
+++ Buzzer.ino
@@ -3,30 +3,45 @@
 const int taster1 = 7;
 const int taster2 = 8;
 const int dmxout = 3;
+const int player = 5;
+const int led_on = 10;
+const int led_buzzer = 11;
 int tasterstatus = 0;
 
 void setup() {
   pinMode(taster1, INPUT);
   pinMode(taster2, INPUT);
+  pinMode(player, OUTPUT);
 
   DmxSimple.usePin(3);
   DmxSimple.maxChannel(4);
 
   Serial.begin(9600);
+
+  digitalWrite(player, HIGH);
+  digitalWrite(led_on, HIGH);
 }
 
 void loop() {
+  
   if (tasterstatus == 0 && digitalRead(taster1) == HIGH) {
     tasterstatus = 1;
+    digitalWrite(led_buzzer, HIGH);
     DmxSimple.write(1, 255); //DMX Kanal 1 auf 255
-    //Sound abspielen
+    digitalWrite(player, LOW);
+    delay(100);
+    digitalWrite(player, HIGH);
     delay (2000);
+    digitalWrite(led_buzzer, LOW);
     tasterstatus = 0;
   }
   if (tasterstatus == 0 && digitalRead(taster2) == HIGH) {
     tasterstatus = 1;
+    digitalWrite(led_buzzer, HIGH);
     DmxSimple.write(2, 255); //DMX Kanal 2 auf 255
-    //Sound abspielen
+    digitalWrite(player, LOW);
+    delay(100);
+    digitalWrite(player, LOW);
     delay (2000);
     tasterstatus = 0;
   }
