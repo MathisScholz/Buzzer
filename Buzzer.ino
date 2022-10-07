df6303c0d7f951eaced1cd5711729bdedc090f67
Mathis Scholz <mail@mathis-scholz.de>
2022-10-07 19:08:20 +0200

    second buzzer

--- Buzzer.ino
+++ Buzzer.ino
@@ -18,31 +18,32 @@ void setup() {
 
   Serial.begin(9600);
 
-  digitalWrite(player, HIGH);
-  digitalWrite(led_on, HIGH);
+  digitalWrite(player, HIGH); //active-low
+  digitalWrite(led_on, HIGH); //status led - system running
 }
 
 void loop() {
   
   if (tasterstatus == 0 && digitalRead(taster1) == HIGH) {
     tasterstatus = 1;
-    digitalWrite(led_buzzer, HIGH);
+    digitalWrite(led_buzzer, HIGH); //status led - buzzer hit
     DmxSimple.write(1, 255); //DMX Kanal 1 auf 255
     digitalWrite(player, LOW);
     delay(100);
-    digitalWrite(player, HIGH);
+    digitalWrite(player, HIGH); 
     delay (2000);
     digitalWrite(led_buzzer, LOW);
     tasterstatus = 0;
   }
   if (tasterstatus == 0 && digitalRead(taster2) == HIGH) {
     tasterstatus = 1;
-    digitalWrite(led_buzzer, HIGH);
+    digitalWrite(led_buzzer, HIGH); //status led - buzzer hit
     DmxSimple.write(2, 255); //DMX Kanal 2 auf 255
     digitalWrite(player, LOW);
     delay(100);
-    digitalWrite(player, LOW);
+    digitalWrite(player, HIGH); 
     delay (2000);
+    digitalWrite(led_buzzer, LOW);
     tasterstatus = 0;
   }
 
